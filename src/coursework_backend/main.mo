a0861815b1c26ea3a8747dc6df90cfacfc06dc61
PandaKewt <chuminhtuan30306@gmail.com>
2022-07-31 11:39:26 +0700

    d

--- src/coursework_backend/main.mo
+++ src/coursework_backend/main.mo
@@ -1,5 +1,49 @@
+import HashMap "mo:base/HashMap";
+import Iter "mo:base/Iter";
+import Nat "mo:base/Nat";
+import Hash "mo:base/Hash";
+import Option "mo:base/Option"
+
 actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+  public type Customer = {
+    name : Text;
+    age : Text;
+    phone : Text;
+    sex : Bool;
+    address : Text;
   };
+
+  stable var upgradeCanisters : [(Nat, Customer)] = [];
+  var customerList = HashMap.HashMap<Nat, Customer>(0, Nat.equal, Hash.hash);
+  stable var next = 0;
+
+  system func preupgrade() {
+    upgradeCanisters := Iter.toArray(customerList.entries());
+  };
+
+  system func postupgrade() {
+    customerList := HashMap.fromIter<Nat, Customer>(upgradeCanisters.vals(), 0, Nat.equal, Hash.hash);
+    upgradeCanisters := [];
+  };
+
+  public func createAccount(cus: Customer)  {
+    customerList.put(next, cus);
+    next += 1;
+  };
+  
+  public func readAccount() : async [(Nat, Customer)] {
+    var returnValue : [(Nat, Customer)] = [];
+    returnValue := Iter.toArray(customerList.entries());
+    return(returnValue);
+  };
+
+  public func updateAccount(id : Nat, cus : Customer) : async ?Customer {
+    return customerList.replace(id, cus)
+  };
+
+  public func deleteAccount(id : Nat) : async ?Customer {
+    return customerList.remove(id)
+  };
+
 };
+
--- src/coursework_frontend/src/index.js
+++ src/coursework_frontend/src/index.js
@@ -1,19 +1,82 @@
-import { coursework_backend } from "../../declarations/coursework_backend";
+import { coursework_backend, canisterId, idlFactory } from "../../declarations/coursework_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+const PlugWallet = window.ic?.plug;
+const whitelist = [canisterId];
+const host = document.location.origin;
+let backendActor = false;
+let id = 0;
 
-  const name = document.getElementById("name").value.toString();
 
-  button.setAttribute("disabled", true);
+const onUpdateCallback = () => {
+  
+}
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await coursework_backend.greet(name);
+document.querySelector("#connect").addEventListener("click", async (e) => {
 
-  button.removeAttribute("disabled");
+  const publicKey = await PlugWallet?.requestConnect({
+    whitelist,
+    host,
+    onUpdateCallback
+  });
 
-  document.getElementById("greeting").innerText = greeting;
+  backendActor = await PlugWallet.createActor({
+    canisterId: canisterId,
+    interfaceFactory: idlFactory
+  });
 
+  var accountList = await backendActor.readAccount();
+  console.log(accountList)
+  for(let i = 0; i < accountList.length; i++) {
+    const account = accountList[i][1];
+    const accountId = accountList[i][0];
+    var sex = "Female"
+    if(account.sex){
+      sex = "Male"
+    } 
+    console.log(account);
+    document.querySelector("table").innerHTML += "<tr><td>"  + account.name + "</td><td>" + account.age + "</td><td>" + account.phone + "</td><td>" + sex + '</td><td> <input type="button" value="update" id="update'+accountId+'"> <input type="button" value="delete" id="delete'+accountId+'"> </td></tr>'
+  }
+  
   return false;
 });
+
+
+document.querySelector("#register").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  let firstName = document.getElementById("firstNameCreate").value;
+  let lastName = document.getElementById("lastNameCreate").value;
+  let age = document.getElementById("birthCreate").value;
+  let phone = document.getElementById("phoneCreate").value;
+  let address = document.getElementById("addressCreate").value;
+  let sex = document.getElementById("maleCreate").checked;
+  let name =  firstName + ' ' + lastName
+  console.log(backendActor)
+  const register = backendActor?.createAccount({
+    age,
+    name,
+    address,
+    sex,
+    phone,
+  })
+})
+
+document.querySelector("#update").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  let firstName = document.getElementById("firstNameUpdate").value;
+  let lastName = document.getElementById("lastNameUpdate").value;
+  let age = document.getElementById("birthUpdate").value;
+  let phone = document.getElementById("phoneUpdate").value;
+  let address = document.getElementById("addressUpdate").value;
+  let sex = document.getElementById("maleUpdate").checked;
+  let name =  firstName + ' ' + lastName
+  console.log(backendActor)
+  const register = backendActor?.createAccount({
+    age,
+    name,
+    address,
+    sex,
+    phone,
+  })
+})
